485cf3940cd527cac0fd9bab50aa79d9606c4aa0
kiran76 <ksknew76@gmail.com>
2024-07-31 20:33:38 +0530

    text size for mobile responsive cards

--- client/components/Bookings.js
+++ client/components/Bookings.js
@@ -10,8 +10,8 @@ const Bookings = ({ bookings }) => {
       <div className="flex w-full md:w-[24.25%] flex-col rounded-lg p-4 shadow-md hover:bg-gray-200">
         <div className="flex gap-[13.5rem] md:gap-[4rem]">
           <div className='block md:flex md:flex-col md:justify-between'>
-            <h2 className="text-[0.9rem] md:text-[0.85rem]">Total Bookings</h2>
-            <p className="text-[1rem] md:text-[0.94rem] font-bold">{bookings.total} Bookings</p>
+            <h2 className="text-[0.7rem] w-[130%] md:w-fit md:text-[0.85rem]">Total Bookings</h2>
+            <p className="text-[0.8rem] md:text-[0.94rem] w-[130%] md:w-fit font-bold">{bookings.total} Bookings</p>
           </div>
           <Image alt='i4' src={f1} width={50} />
         </div>
@@ -19,8 +19,8 @@ const Bookings = ({ bookings }) => {
       <div className="flex w-full md:w-[24%] flex-col rounded-lg p-4 shadow-md hover:bg-gray-200">
         <div className="flex gap-[12.5rem] md:gap-[4rem]">
           <div className='block md:flex md:flex-col md:justify-between'>
-            <h2 className="text-[0.9rem] md:text-[0.7rem]">Bookings Utilized</h2>
-            <p className="text-[1rem] md:text-[1rem] font-bold">{bookings.utilized} Bookings</p>
+            <h2 className="text-[0.7rem] md:text-[0.7rem] w-[130%] md:w-fit">Bookings Utilized</h2>
+            <p className="text-[1rem] md:text-[1rem] font-bold w-[130%] md:w-fit">{bookings.utilized} Bookings</p>
           </div>
           <Image alt='i3' src={f2} height={50} />
         </div>
@@ -28,17 +28,17 @@ const Bookings = ({ bookings }) => {
       <div className="flex w-full md:w-[24%] flex-col rounded-lg p-4 shadow-md hover:bg-gray-200">
         <div className="flex gap-[12.5rem] md:gap-[4rem]">
           <div className='block md:flex md:flex-col md:justify-between'>
-            <h2 className="text-[0.9rem] md:text-[0.65rem]">Booking Cancelled</h2>
-            <p className="text-[1rem] md:text-[1rem] font-bold">{bookings.cancelled} Bookings</p>
+            <h2 className="text-[0.7rem] md:text-[0.65rem] w-[130%] md:w-fit">Booking Cancelled</h2>
+            <p className="text-[1rem] md:text-[1rem] font-bold w-[130%] md:w-fit">{bookings.cancelled} Bookings</p>
           </div>
           <Image alt='i1' src={f3} height={50} />
         </div>
       </div>
       <div className="flex w-full md:w-[24%] flex-col rounded-lg p-4 shadow-md hover:bg-gray-200">
-        <div className="flex gap-[15.5rem] md:gap-[7rem]">
+        <div className="flex gap-[14rem] md:gap-[7rem]">
           <div className='block md:flex md:flex-col md:justify-between'>
-            <h2 className="text-[0.9rem] md:text-[0.65rem]">Utilization</h2>
-            <p className="text-[1rem] md:text-[1rem] font-bold">{bookings.utilization}%</p>
+            <h2 className="text-[0.9rem] md:text-[0.65rem] w-[130%] md:w-fit">Utilization</h2>
+            <p className="text-[1rem] md:text-[1rem] font-bold w-[130%] md:w-fit">{bookings.utilization}%</p>
           </div>
           <Image alt='i2' src={f4} height={50} />
         </div>
